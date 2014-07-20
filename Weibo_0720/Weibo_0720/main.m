e8da2c3aa3d73c732c66650951cde1b6ed396f45
panda <pandabo1985@gmail.com>
2014-07-20 20:15:35 +0800

    weibo practice begin

---
+++ Weibo_0720/Weibo_0720/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Weibo_0720
+//
+//  Created by pan dabo on 14-7-20.
+//  Copyright (c) 2014年 afayear. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ Weibo_0720/Weibo_0720Tests/Weibo_0720Tests.m
@@ -0,0 +1,34 @@
+//
+//  Weibo_0720Tests.m
+//  Weibo_0720Tests
+//
+//  Created by pan dabo on 14-7-20.
+//  Copyright (c) 2014年 afayear. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface Weibo_0720Tests : XCTestCase
+
+@end
+
+@implementation Weibo_0720Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
